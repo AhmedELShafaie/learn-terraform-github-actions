54bdc4f58826f922dafe11ff127c0a2c874dda0b
Ahmed <ahmed@loc.kr>
2022-05-25 17:23:18 +0200

    Add terraform cloud as backend

--- main.tf
+++ main.tf
@@ -11,16 +11,18 @@ terraform {
       source  = "hashicorp/random"
       version = "3.0.1"
     }
+    backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "samir-toptal"
+
+    workspaces {
+      name = "samir-toptal-workspace"
+    }
+  }
   }
   required_version = ">= 1.1.0"
 
-  cloud {
-    organization = "my-learn-terraform-aws"
-
-    workspaces {
-      name = "gh-actions-demo"
-    }
-  }
+  
 }
 
 provider "aws" {
